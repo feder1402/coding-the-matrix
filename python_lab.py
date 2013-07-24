8795faa6a19608447d6660234fc999f76b8ef1c7
Federico Raggi <federicoraggi@hotmail.com>
2013-07-24 13:37:25 -0700

    Python lab exercises 10 to 24 included

--- python_lab.py
+++ python_lab.py
@@ -37,57 +37,57 @@ T = {3, 4, 5, 6}
 S_intersect_T = { x for x in S|T if (x in S and x in T) }
 
 ## Task 11
-L_average = ... # average of: [20, 10, 15, 75]
+L_average = sum([20, 10, 15, 75])/len([20, 10, 15, 75]) # average of: [20, 10, 15, 75]
 
 ## Task 12
 LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
-LofL_sum = ... # use form: sum([sum(...) ... ])
+LofL_sum = sum([sum(x) for x in LofL]) # use form: sum([sum(...) ... ])
 
 ## Task 13
-cartesian_product = ... # use form: [ ... {'A','B','C'} ... {1,2,3} ... ]
+cartesian_product = [[x, y] for x in {'A','B','C'} for y in {1,2,3}] # use form: [ ... {'A','B','C'} ... {1,2,3} ... ]
 
 ## Task 14
 S = {-4, -2, 1, 2, 5, 0}
-zero_sum_list = [ ... ] 
+zero_sum_list = [(x, y, z) for x in S for y in S for z in S if x + y + z == 0]
 
 ## Task 15
-exclude_zero_list = [ ... ]
+exclude_zero_list = [(x, y, z) for x in S for y in S for z in S if (x + y + z == 0) and ((x,y,z) != (0,0,0))]
 
 ## Task 16
-first_of_tuples_list = ...
+first_of_tuples_list = [(x, y, z) for x in S for y in S for z in S if (x + y + z == 0) and ((x,y,z) != (0,0,0))][0]
 
 ## Task 17
-L1 = [...] # <-- want len(L1) != len(list(set(L1)))
-L2 = [...] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
+L1 = [1,1,2,3] # <-- want len(L1) != len(list(set(L1)))
+L2 = [5, 2, 1, 7, 10] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
 
 ## Task 18
-odd_num_list_range = {...}
+odd_num_list_range = {x for x in range(100) if x%2 !=0}
 
 ## Task 19
 L = ['A','B','C','D','E']
-range_and_zip = ...
+range_and_zip = list(zip(range(5), L))
 
 ## Task 20
-list_sum_zip = [...]
+list_sum_zip = [x+y for [x,y] in zip([10, 25, 40], [1, 15, 20])]
 
 ## Task 21
 dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'Lewis'}, {'James':'Pierce', 'director':'Roger'}]
 k = 'James'
-value_list = [...]
+value_list = [x[k] for x in dlist]
 
 ## Task 22
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
-value_list_modified_1 = [...] # <-- Use the same expression here
+value_list_modified_1 = [x[k] if k in x else 'NOT PRESENT' for x in dlist] # <-- Use the same expression here
 k = 'Frodo'
-value_list_modified_2 = [...] # <-- as you do here
+value_list_modified_2 = [x[k] if k in x else 'NOT PRESENT' for x in dlist] # <-- as you do here
 
 ## Task 23
-square_dict = {...}
+square_dict = {x:x**2 for x in range(100)}
 
 ## Task 24
 D = {'red','white','blue'}
-identity_dict = {...}
+identity_dict = {x:x for x in D}
 
 ## Task 25
 base = 10
